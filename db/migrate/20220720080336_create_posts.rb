9b513d74880a65d9c33f058f23e5db2a746d320b
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-20 17:06:12 +0900

    PostモデルにNonnull制約、デフォルト値の設定追加

--- db/migrate/20220720080336_create_posts.rb
+++ db/migrate/20220720080336_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :present_name
-      t.integer :present_score
-      t.string :present_review
+      t.string :present_name, null: false
+      t.integer :present_score, null: false, default: 1
+      t.string :present_review, null: false
 
       t.timestamps
     end
