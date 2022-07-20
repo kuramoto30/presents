c2547a0ddc5dd7e1012583266939e6bc73f7b563
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-20 17:16:42 +0900

    usersテーブル作成

--- db/migrate/20220720081257_create_users.rb
+++ db/migrate/20220720081257_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :nickname
-      t.integer :gender
-      t.integer :age
+      t.string :nickname, null: false
+      t.integer :gender, null: false, default: 0
+      t.integer :age, null: false, default: 0
 
       t.timestamps
     end
