d92ab104bc66658200f771c2a97b54b68a0a67e9
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-21 10:57:11 +0900

    usersルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   root "posts#index"
   resources :posts
+  resources :users
 end
