bb415a533d9c6ace75d8aeaaa2363c435a55eed0
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-21 10:55:52 +0900

    usersコントローラー、ビューファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   root "posts#index"
   resources :posts
 end
