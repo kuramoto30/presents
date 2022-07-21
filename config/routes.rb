c07b6fdf4092bb0ed518064d299855c00fb4a4f1
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-21 10:32:40 +0900

    postsコントローラー、ビューファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'post/index'
+  get 'post/new'
+  get 'post/create'
+  get 'post/show'
+  get 'post/edit'
+  get 'post/update'
+  get 'post/destroy'
 end
