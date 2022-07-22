29875c337dc8739476ac49cc8bd6b992a187b2cc
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-22 14:08:22 +0900

    rubocopによる修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_21_021925) do
-
+ActiveRecord::Schema.define(version: 20_220_721_021_925) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "posts", force: :cascade do |t|
-    t.string "present_name", null: false
-    t.integer "present_score", default: 1, null: false
-    t.string "present_review", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'posts', force: :cascade do |t|
+    t.string 'present_name', null: false
+    t.integer 'present_score', default: 1, null: false
+    t.string 'present_review', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.integer "gender", default: 0, null: false
-    t.integer "age", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'nickname', null: false
+    t.integer 'gender', default: 0, null: false
+    t.integer 'age', default: 0, null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(present_name: 'パソコン', present_score: 5, present_review: 'プログラミングが出来るので嬉しかった。')
+Post.create!(present_name: 'フォーク', present_score: 1, present_review: '意味がわからなかった。')
+Post.create!(present_name: '腕時計', present_score: 3, present_review: '嬉しいけど好きなブランドでなかった。')
+Post.create!(present_name: 'コーヒー豆', present_score: 4, present_review: 'よく飲むから嬉しかった。でも誕生日にはちょっと、、、')
+Post.create!(present_name: 'お肉', present_score: 2, present_review: '固くて不味かった。気持ちは嬉しかった。')
+Post.create!(present_name: '低音調理器具', present_score: 4, present_review: '嬉しいけど使い方わからない。')
+
+puts '初期データの投入に成功'
