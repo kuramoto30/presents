70012c0f3c0f94ec760011fe1f87f20fc2b8a655
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-22 14:52:14 +0900

    ユーザーアカウント編集項目の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
   end
@@ -13,26 +16,26 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to post
+    @post.update!(post_params)
+    redirect_to  post_path
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy!
+    @post.destroy!
     redirect_to root_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:present_name, :present_score, :present_review)
