1d4ade629935011d7326516492dacc17f94a9b1e
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-24 13:56:21 +0900

    新規投稿ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(post_params)
+    post = current_user.posts.create!(post_params)
     redirect_to post
   end
 
