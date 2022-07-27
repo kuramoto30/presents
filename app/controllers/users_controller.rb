5a91ab7ae3d5821b974b2f11d7e070cb7f14e7f4
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-27 12:31:47 +0900

    ログインユーザーのkeep一覧実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    keeps = Keep.where(user_id: @user.id).pluck("post_id DESC")
+    @keep_posts = Post.find(keeps)
     @posts = @user.posts.order("id DESC")
   end
 
