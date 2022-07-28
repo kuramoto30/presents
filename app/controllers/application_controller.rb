66ad710829730b56e8204c63f639da4a8a5dcd38
kuramoto30 <exclusive.kuramoto@gmail.com>
2022-07-28 20:16:36 +0900

    Navbarに検索フォームを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_search
+  PER_PAGE = 10
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname gender age])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[nickname gender age])
+  end
+
+  def set_search
+    @q = Post.ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(PER_PAGE).limit(20)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_search
   before_action :set_post, only: %i[show edit update destroy]
   PER_PAGE = 10
 
